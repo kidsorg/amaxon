a1398383e6bb0adbf1e252c50359cd0018752341
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2025-03-04 22:17:00 +0530

    syso added in test.py file

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 test1 file created
 public static void main(String[] args){
- System.out.println("test 1 file created")
+ System.out.println("test 1 file created");
+ System.out.println("I want to practice java because it is important bro")
         }
 
