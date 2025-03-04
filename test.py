3d273dd30a12e9d99171d91f6fc7f330f941d948
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2025-03-04 22:25:27 +0530

    java commmit

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 test1 file created
 public static void main(String[] args){
- System.out.println("test 1 file created")
+ System.out.println("test 1 file created");
+ System.out.println("I love java bro, It is interesting with sprint bean and sprint boot and rest and mvc")
         }
 
