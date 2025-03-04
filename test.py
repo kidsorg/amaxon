979685377def334a5f2e9493b87fa8a979bde7ef
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2025-03-04 22:19:18 +0530

    public static void main(String[]args){ System.out.println("revert worked or not"); }

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 test1 file created
 public static void main(String[] args){
- System.out.println("test 1 file created");
- System.out.println("I want to practice java because it is important bro")
+ System.out.println("test 1 file created")
         }
 
