4d45ceeb92a9d9b48ba7a16dd57f0a15dd063768
prasadbodduarena <prasadnaiduboddu081@gmail.com>
2025-03-04 22:26:02 +0530

    Revert "java commmit"
    This reverts commit 3d273dd30a12e9d99171d91f6fc7f330f941d948.

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 test1 file created
 public static void main(String[] args){
- System.out.println("test 1 file created");
- System.out.println("I love java bro, It is interesting with sprint bean and sprint boot and rest and mvc")
+ System.out.println("test 1 file created")
         }
 
